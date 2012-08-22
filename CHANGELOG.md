7b302a7ead0bcbc505f61a23ab83170ed458906c
jtimberman <joshua@opscode.com>
2012-08-22 15:29:19 -0600

    release v1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## v1.0.0:
+
+* [COOK-1529] - add default logwatch.conf
+* add test-kitchen support
+
 ## v0.1.1:
 
 * Current released version.
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs logwatch, a nice log analyzer"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "1.0.0"
 
 depends "perl"
 
