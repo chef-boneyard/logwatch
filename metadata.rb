b1d0c694fe8fdc26aeeff976f29dd3b47034e78c
jtimberman <joshua@opscode.com>
2012-10-22 15:59:27 -0700

    release v1.0.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs logwatch, a nice log analyzer"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.2"
 
 depends "perl"
 
