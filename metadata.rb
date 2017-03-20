960a7c5bef71a4f2001626f1956819e97ef9bc21
Tim Smith <tsmith@chef.io>
2017-03-20 12:52:12 -0700

    Loosen the perl dependency
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
 name 'logwatch'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs logwatch, a nice log analyzer'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.1.0'
 
-depends 'perl', '~> 1.2'
+depends 'perl', '>= 1.2'
 
 %w(ubuntu debian redhat centos scientific oracle amazon).each do |os|
   supports os
