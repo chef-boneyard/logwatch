91737734d9ed1c1721c7dce2f017177debffd2ae
Tim Smith <tsmith84@gmail.com>
2019-10-07 15:23:48 -0700

    Resolve Cookstyle 5.8 warnings
    Minor cleanups for the latest Cookstyle
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs logwatch, a nice log analyzer'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.1.1'
 
 depends 'perl', '>= 1.2'
@@ -11,8 +10,6 @@ depends 'perl', '>= 1.2'
 %w(ubuntu debian redhat centos scientific oracle amazon).each do |os|
   supports os
 end
-
-recipe 'logwatch', 'Installs logwatch'
 
 source_url 'https://github.com/chef-cookbooks/logwatch'
 issues_url 'https://github.com/chef-cookbooks/logwatch/issues'
